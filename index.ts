e63c70bf9019133a759be59120daab155860c91d
codehz <git@hertz.moe>
2024-07-06 22:31:30 +0800

    prevent use without macro

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
+if (typeof window !== "undefined") {
+  throw new Error(
+    "bun-tailwindcss is not supported in browsers. Please import as macro."
+  );
+}
+
 const channel = new BroadcastChannel("bun-tailwindcss");
 
 export function tw(input: string) {
