b4c4dccb9dd4edee939b8fa9618c59be3fc8b39c
codehz <git@hertz.moe>
2024-10-02 11:26:46 +0800

    upgrade tailwindcss to 4.0.0-alpha.25

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to this project will be documented in this file.
 
+## 0.0.7
+
+- upgrade tailwindcss to 4.0.0-alpha.25
+
 ## 0.0.6
 
 - upgrade tailwindcss to 4.0.0-alpha.19
--- build.ts
+++ build.ts
@@ -1,5 +1,16 @@
+import EnhancedResolve from "enhanced-resolve";
+import fs from "node:fs";
+import { dirname, extname } from "node:path";
 import { compile } from "tailwindcss";
 import type { MessageType } from "./types";
+
+const cssResolver = EnhancedResolve.ResolverFactory.createResolver({
+  fileSystem: new EnhancedResolve.CachedInputFileSystem(fs, 4000),
+  useSyncFileSystemCalls: true,
+  extensions: [".css"],
+  mainFields: ["style"],
+  conditionNames: ["style"],
+});
 
 export class TailwindCSSCollector {
   #channel = new BroadcastChannel("bun-tailwindcss");
@@ -29,11 +40,68 @@ export class TailwindCSSCollector {
       ([name, contents]) => `.${name} { @apply ${contents.join(" ")}; }`
     );
     const css = [this.css, ...generatedClasses].join("\n");
-    const compiled = await compile(css);
+    const compiled = await compile(css, { loadModule, loadStylesheet });
     return compiled.build([...this.#candidates]);
   }
   [Symbol.dispose]() {
     this.#channel.removeEventListener("message", this.#listener);
     this.#channel.close();
   }
+}
+
+async function loadModule(
+  id: string,
+  base: string
+): Promise<{
+  module: any;
+  base: string;
+}> {
+  const resolvedPath = Bun.fileURLToPath(import.meta.resolve(id, base));
+  const module = await import(resolvedPath);
+  return { module: module.default ?? module, base: dirname(resolvedPath) };
+}
+
+async function loadStylesheet(
+  id: string,
+  base: string
+): Promise<{
+  content: string;
+  base: string;
+}> {
+  const resolvedPath = await resolveCssId(id, base);
+  if (!resolvedPath)
+    throw new Error(`Could not resolve '${id}' from '${base}'`);
+
+  const content = await Bun.file(resolvedPath).text();
+  return { content, base: dirname(resolvedPath) };
+}
+
+async function resolveCssId(
+  id: string,
+  base: string
+): Promise<string | false | undefined> {
+  const skipRelativeCheck = extname(id) === "" || id.startsWith(".");
+
+  if (!skipRelativeCheck) {
+    try {
+      const dotResolved = await runResolver(cssResolver, `./${id}`, base);
+      if (!dotResolved) throw new Error();
+      return dotResolved;
+    } catch {}
+  }
+
+  return runResolver(cssResolver, id, base);
+}
+
+function runResolver(
+  resolver: EnhancedResolve.Resolver,
+  id: string,
+  base: string
+): Promise<string | false | undefined> {
+  return new Promise((resolve, reject) =>
+    resolver.resolve({}, base, id, {}, (err, result) => {
+      if (err) return reject(err);
+      resolve(result);
+    })
+  );
 }
